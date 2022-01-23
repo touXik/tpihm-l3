2e175f3ef668dadb67db20ed034b4980c7fdc91b
toufik <toufikmedjber2@gmail.com>
2022-01-23 22:42:12 +0100

    version2

--- js/js.js
+++ js/js.js
@@ -1,39 +1,3 @@
-// slides--------------------------------------------------------
-
-       let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let slides = document.getElementsByClassName('slides');
-  let dots = document.getElementsByClassName('dot');
-  
-  if(n > slides.length) { slideIndex = 1 }
-  
-  if(n < 1 ) { slideIndex = slides.length }
-  
-
-  for(let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  
-
-  for(let i = 0; i < dots.length; i++) {
-    dots[i].classList.remove('active');
-  }
-  
-
-  slides[slideIndex - 1].style.display = 'block';
-  
-  dots[slideIndex - 1].classList.add('active');
-}
 
 
 
@@ -48,12 +12,6 @@ toggle.addEventListener('click', () => {
 
 //------------------------------------------------------------------------------
 
-const burgerContainer = document.querySelector('.burger-container');
-        const navLinks = document.querySelector('.nav-links');
 
-        burgerContainer.addEventListener('click', () => {
-            burgerContainer.classList.toggle('active');
-            navLinks.classList.toggle('active');
-        })
 
       
