5fe16267953486207d4c34b422e7b0264111b349
toufik <toufikmedjber2@gmail.com>
2022-01-25 01:22:06 +0100

    version3

--- js/js.js
+++ js/js.js
@@ -12,6 +12,13 @@ toggle.addEventListener('click', () => {
 
 //------------------------------------------------------------------------------
 
+const burgerContainer = document.querySelector('.burger-container');
+        const navLinks = document.querySelector('.nav-links');
+
+        burgerContainer.addEventListener('click', () => {
+            burgerContainer.classList.toggle('active');
+            navLinks.classList.toggle('active');
+        })
 
 
       
